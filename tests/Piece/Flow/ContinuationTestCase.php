7106882fb2dcaa43412cf0edd6ab0d3aa1e734df
KUBO Atsuhiro <kubo@iteman.jp>
2006-06-13 01:36:57 +0000

    - Adjusted to changes in the Piece_Flow_Continuation class.

--- tests/Piece/Flow/ContinuationTestCase.php
+++ tests/Piece/Flow/ContinuationTestCase.php
@@ -41,7 +41,6 @@
 require_once 'PHPUnit.php';
 require_once 'Piece/Flow/Continuation.php';
 require_once 'PEAR/ErrorStack.php';
-require_once 'Piece/Flow/Action/Factory.php';
 require_once 'Piece/Flow/Error.php';
 require_once 'Cache/Lite/File.php';
 
@@ -87,7 +86,7 @@ class Piece_Flow_ContinuationTestCase extends PHPUnit_TestCase
     function setUp()
     {
         PEAR_ErrorStack::staticPushCallback(create_function('$error', 'var_dump($error); return ' . PEAR_ERRORSTACK_DIE . ';'));
-        Piece_Flow_Action_Factory::setActionDirectory(dirname(__FILE__) . '/../..');
+        Piece_Flow_Continuation::setActionDirectory(dirname(__FILE__) . '/../..');
         $this->_flowName = 'Counter';
         $this->_eventName = 'increase';
     }
