a7b92a77105ca42dff251f9e508b1552f3dab231
KUBO Atsuhiro <kubo@iteman.jp>
2013-08-21 17:34:57 +0900

    [PageFlow] changed the base class of the EventNotFoundException class to LogicException

--- src/Piece/Flow/PageFlow/EventNotFoundException.php
+++ src/Piece/Flow/PageFlow/EventNotFoundException.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5.3
  *
- * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2012-2013 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Piece_Flow
- * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2012-2013 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      File available since Release 2.0.0
@@ -39,12 +39,12 @@ namespace Piece\Flow\PageFlow;
 
 /**
  * @package    Piece_Flow
- * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2012-2013 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      Class available since Release 2.0.0
  */
-class EventNotFoundException extends \RuntimeException
+class EventNotFoundException extends \LogicException
 {
 }
 
